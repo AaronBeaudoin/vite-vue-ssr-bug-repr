5c5ba4c2f5afcc62dc06d24873b1bfaea297aae4
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-02-02 11:45:25 -0600

    Made example a tad more minimal

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <script setup>
-import { ref } from "vue";
 import Test from "./Test.vue";
-
-let test = ref(false);
-setTimeout(_ => test.value = true, 0);
 </script>
 
 <template>
   <Test>
     <transition>
-      <div v-if="test">
-        Hello minimal reproduction!
-      </div>
+      <div v-if="false"></div>
     </transition>
   </Test>
 </template>
